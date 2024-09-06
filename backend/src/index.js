6aa049f4ad4ca77d39f55fc3ab0c34bcf8e2a4c4
Tajamulhussain02 <tajamul@juw.edu.pk>
2024-09-05 23:44:58 -0700

    feat; usermanagement and auth api integrated

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 // const mysql = require('mysql2');
 const app = express();
 const port = process.env.PORT || 5000;
-
+const cors = require('cors');
 const userRouter = require('../routes/userRoutes')
 const authRouter = require('../routes/authRoute')
 const publicationRouter = require('../routes/publicationRoute');
@@ -11,10 +11,14 @@ const Department = require('../Models/departmentmodel');
 const isAdminorManager = require('../middlewares/isAdminorManager');
 const authenticate = require('../middlewares/auth');
 app.use(express.json()); // Middleware to parse JSON bodies
+app.use(cors())
+
 
 require('../Models/association')
 // Import the sequelize instance to ensure the connection is established
 require('../config/mysqlConnection'); // Ensure this line is present
+
+
 
 // app.use(authenticate);
 // app.use(isAdminorManager);
@@ -22,6 +26,7 @@ app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 app.use('/api/publication', publicationRouter);
 app.use('/api/intellectualproperty', intellectualproperty);
+
 
 // require('../routes')
 // Department.bulkCreate([
